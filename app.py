cdb88b6c89715b46241638a154dbeeca1112ea2b
Abhishek Sharma <abhisheksharma@Abhisheks-MacBook-Air.local>
2025-01-21 18:41:34 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from textblob import TextBlob
+# Initialize Flask app
+app = Flask(__name__)
+
+@app.route("/analyze", methods=["POST"])
+def analyze_sentiment():
+    try:
+        # Get the JSON data from the request
+        data = request.get_json()
+        text = data.get("text", "")
+
+        if not text:
+            return jsonify({"error": "No text provided"}), 400
+
+        # Perform sentiment analysis
+        blob = TextBlob(text)
+
+        polarity = blob.sentiment.polarity
+        results = "Neutral"
+        # Determine sentiment based on polarity
+        if polarity > 0:
+            results = "Positive"
+        elif polarity < 0:
+            results = "Negative"
+        else:
+            results =  "Neutral"
+        return jsonify({"results": results}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# Run the Flask app
+if __name__ == "__main__":
+    app.run(debug=True)
